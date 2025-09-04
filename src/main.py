a2e0c0395e213589b3289259907b9d6be1d55e93
susTuna <hidasetsuna@gmail.com>
2025-09-05 04:18:15 +0700

    feat: title list paginator

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import discord
 import os
-from GalleryPaginator import GalleryPaginator
+from gallerypaginator import GalleryPaginator
+from listpaginator import ListPaginator
 from discord.ext import commands
 from scraper import scrape_gallery, scrape_title
 from dotenv import load_dotenv
@@ -19,20 +20,29 @@ async def search(ctx, *, query: str):
     if query.isdigit():
         await ctx.send(f"🔍 Searching gallery {query}...")
         result = await scrape_gallery(query)
+
+        if result is None:
+            await ctx.send("❌ Failed to fetch gallery!")
+            return
+
+        num_pages, title, tags, image_urls = result
+
+        embed = discord.Embed(title=f"{title} - {num_pages} Pages", description=f"Use the buttons below to scroll\n\n{", ".join(tags)}", color=0xFF0000)
+        embed.set_image(url=image_urls[0])
+        embed.set_footer(text=f"Page 1/{num_pages}")
+
+        view = GalleryPaginator(ctx, title, tags, image_urls)
+        await ctx.send(embed=embed, view=view)
     else:
         await ctx.send(f"🔍 Searching for {query}...")
-        result = await scrape_title(query)
-    if result is None:
-        await ctx.send("❌ Failed to fetch gallery!")
-        return
-
-    media_id, num_pages, title, tags, image_urls = result
-
-    embed = discord.Embed(title=f"{title} - {num_pages} Pages", description=f"Use the buttons below to scroll\n\n{", ".join(tags)}", color=0xFF0000)
-    embed.set_image(url=image_urls[0])
-    embed.set_footer(text=f"Page 1/{num_pages}")
-
-    view = GalleryPaginator(ctx, title, tags, image_urls)
-    await ctx.send(embed=embed, view=view)
+        gallery_pairs = await scrape_title(query)
+        
+        if not gallery_pairs:
+            await ctx.send(f"❌ No results found for '{query}'.")
+            return
+            
+        view = ListPaginator(ctx, gallery_pairs)
+        embed = view.create_list_embed()
+        await ctx.send(embed=embed, view=view)
     
 bot.run(TOKEN)

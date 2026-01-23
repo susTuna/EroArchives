1243d1ac4c92171a3fc6463c10f38efd13629a46
susTuna <hidasetsuna@gmail.com>
2026-01-23 17:26:31 +0700

    feat: fixing dns over https

--- src/doh.py
+++ src/doh.py
@@ -1,13 +1,23 @@
 import os
-from aiohttp import TCPConnector
-from aiohttp.resolver import AsyncResolver
+import httpx
+import dns.resolver
 from dotenv import load_dotenv
 
 load_dotenv()
 
 NS = [ns.strip() for ns in os.getenv("DNS_SERVERS").split(',')]
 
+def config_ns_resolv():
+    resolv = dns.resolver.Resolver()
+    resolv.nameservers = NS
+    dns.resolver.default_resolver = resolv
+    return resolv
+
 def create_doh_connector():
-    resolver = AsyncResolver(nameservers=NS)
-    connector = TCPConnector(resolver=resolver,use_dns_cache=True)
-    return connector
+    config_ns_resolv()
+    
+    return httpx.AsyncClient(
+        timeout=30.0,
+        limits=httpx.Limits(max_keepalive_connections=5, max_connections=10),
+        follow_redirects=True
+    )
--- src/scraper.py
+++ src/scraper.py
@@ -6,52 +6,58 @@ from typing import Optional
 from parser import scraper, parse_cdn_lists, parse_thumbnails, parse_title, parse_pagination
 from balancer import UrlTransformer
 from logger import logger
+from doh import create_doh_connector
 
 async def scrape_gallery(gallery_id: int) -> Optional[tuple]:
     api_url = f"https://nhentai.net/api/gallery/{gallery_id}"
     nhen_url = f"https://nhentai.net/g/{gallery_id}"
     headers = {"User-Agent": "Mozilla/5.0"}
 
-    async with httpx.AsyncClient(headers=headers) as client:
+    client = create_doh_connector()
+    client.headers.update(headers)
+    
+    try:
         api_task = client.get(api_url)
 
         url_task = asyncio.to_thread(scraper.get, nhen_url)
 
         api_response, url_response = await asyncio.gather(api_task, url_task)
 
-    if api_response.status_code != 200 or url_response.status_code != 200:
-        logger.error(f"Failed to fetch data. API Status: {api_response.status_code}, URL Status: {url_response.status_code}")
-        return None
+        if api_response.status_code != 200 or url_response.status_code != 200:
+            logger.error(f"Failed to fetch data. API Status: {api_response.status_code}, URL Status: {url_response.status_code}")
+            return None
 
-    gallery_json = api_response.json()
-    media_id = gallery_json["media_id"]
-    num_pages = gallery_json["num_pages"]
-    title = gallery_json["title"]["english"]
-    tags = [tag["name"] for tag in gallery_json["tags"]]
+        gallery_json = api_response.json()
+        media_id = gallery_json["media_id"]
+        num_pages = gallery_json["num_pages"]
+        title = gallery_json["title"]["english"]
+        tags = [tag["name"] for tag in gallery_json["tags"]]
 
-    thumb_cdn, image_cdn = parse_cdn_lists(url_response.text)
-    balancer = UrlTransformer(thumb_cdn, image_cdn)
-    thumbnail_urls = parse_thumbnails(url_response.text)
+        thumb_cdn, image_cdn = parse_cdn_lists(url_response.text)
+        balancer = UrlTransformer(thumb_cdn, image_cdn)
+        thumbnail_urls = parse_thumbnails(url_response.text)
 
-    image_urls = balancer.transform_list(thumbnail_urls)
+        image_urls = balancer.transform_list(thumbnail_urls)
 
-    return num_pages, title, tags, image_urls
-
+        return num_pages, title, tags, image_urls
+    finally:
+            await client.aclose()
+        
 async def scrape_web(url: str) -> Optional[tuple]:
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     }
 
-    async with httpx.AsyncClient(headers=headers, timeout=30.0) as client:
-        try:
-            logger.info(f"Fetching initial page: {url}")
-            first_page_response = await fetch_url_with_retry(client, url)
-            if not first_page_response:
-                return None
-            first_page_html = first_page_response.text
-        except httpx.RequestError as e:
-            logger.error(f"Failed to fetch initial page {url}: {e}")
+    client = create_doh_connector()
+    client.headers.update(headers)
+    client.timeout = httpx.Timeout(30.0)
+
+    try:
+        logger.info(f"Fetching initial page: {url}")
+        first_page_response = await fetch_url_with_retry(client, url)
+        if not first_page_response:
             return None
+        first_page_html = first_page_response.text
         remaining_page_urls = parse_pagination(first_page_html)
         logger.info(f"Found {len(remaining_page_urls)} additional pages to scrape concurrently.")
         
@@ -86,6 +92,11 @@ async def scrape_web(url: str) -> Optional[tuple]:
         logger.info(f"Total galleries found: {len(gallery_pairs)}")
 
         return gallery_pairs
+    except httpx.RequestError as e:
+        logger.error(f"Failed to fetch initial page {url}: {e}")
+        return None
+    finally:
+        await client.aclose()
 
 async def scrape_title(title: str) -> Optional[tuple]:
     if not title:
